687dbbf8d02aeddf4a01d24a40cd3c090b73c57a
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2017-01-09 16:03:28 -0600

    Installed NeDB, base-laying for layout of the player

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.maximize()
+  mainWindow.setTitle('Nexus')
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
